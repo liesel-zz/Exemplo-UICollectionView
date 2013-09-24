2dc2b1cd1b8cdb6d0da44a2c01f9d7b1a08ba6e1
liesel <joel@cafecomjava.com.br>
2013-09-24 10:03:18 -0300

    Adiciona o arquivo que retorna o json para o aplicativo.

---
+++ categorias.php
@@ -0,0 +1,11 @@
+<?php 
+  header('Content-Type: text/json');
+  echo '{"categoria":[
+  {"nome":"Massas","idcategoria":"2","imagem":"massas.png"},
+  {"nome":"Peixes","idcategoria":"9","imagem":"pexies_frutosdomar.png"},
+  {"nome":"Lanches","idcategoria":"3","imagem":"lanches.png"},
+  {"nome":"Sobremesas","idcategoria":"4","imagem":"sobremesas.png"},
+  {"nome":"Sopas","idcategoria":"8","imagem":"sopa.png"},
+  {"nome":"Porcoes","idcategoria":"5","imagem":"porcoes.png"}
+]}';
+?>
